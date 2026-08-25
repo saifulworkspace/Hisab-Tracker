87aaafd2b75db051d7107499e7e843ebac8f25d4
Saiful <saif.ertqa@gmail.com>
2026-08-25 17:02:15 +0300

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,20 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-function Router() {
+function AppRoutes() {
   return (
-    <Switch>
+    <WouterRouter base={import.meta.env.BASE_URL}>
+      <Switch>
       <Route path="/" component={Home} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
-    </Switch>
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -22,7 +24,7 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <AppRoutes />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
